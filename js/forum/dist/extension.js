f1ebe7a1c75babd78570d89e4e155c31f3349708
Cem Yılmaz <tidusdragonh3art@gmail.com>
2016-10-25 14:26:25 +0300

    qeqweqw

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -17,7 +17,7 @@ System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app', 'flarum
             app.initializers.add('xengine-mdeditor', function () {
                 extend(ComposerBody.prototype, 'init', function init() {
                     console.log(this.editor);
-                    console.log(document.getElementsByClassName('TextEditor'));
+                    console.log(this.editor.element);
                     //var simplemde = new SimpleMDE({element : document.getElementsByClassName('TextEditor')});
                 });
             });
