c2a9a4d03eb504153cec1674d3b57b095f9f2cfb
XEngine <tidusdragonh3art@gmail.com>
2016-10-25 03:11:46 +0300

    Booyah

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,17 +1,23 @@
-System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app'], function (_export) {
+System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app', 'flarum/components/ComposerBody', 'flarum/components/Composer'], function (_export) {
     'use strict';
 
-    var extend, app;
+    var extend, app, ComposerBody, Composer;
     return {
         setters: [function (_flarumExtend) {
             extend = _flarumExtend.extend;
         }, function (_flarumApp) {
             app = _flarumApp['default'];
+        }, function (_flarumComponentsComposerBody) {
+            ComposerBody = _flarumComponentsComposerBody['default'];
+        }, function (_flarumComponentsComposer) {
+            Composer = _flarumComponentsComposer['default'];
         }],
         execute: function () {
 
             app.initializers.add('xengine-mdeditor', function () {
-                alert('Hello, world!');
+                extend(ComposerBody.prototype, 'init', function init() {
+                    console.log(this.editor);
+                });
             });
         }
     };
