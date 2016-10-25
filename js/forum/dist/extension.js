6ee0c2c3405dced57dee2a1c0592efd0667e3112
Cem Yılmaz <tidusdragonh3art@gmail.com>
2016-10-25 13:12:59 +0300

    qeqweqw

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,7 +1,7 @@
-System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app', 'flarum/components/ComposerBody', 'flarum/components/Composer', 'simplemde'], function (_export) {
+System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app', 'flarum/components/ComposerBody', 'flarum/components/Composer'], function (_export) {
     'use strict';
 
-    var extend, app, ComposerBody, Composer, SimpleMDE;
+    var extend, app, ComposerBody, Composer;
     return {
         setters: [function (_flarumExtend) {
             extend = _flarumExtend.extend;
@@ -11,8 +11,6 @@ System.register('xengine/mdeditor/main', ['flarum/extend', 'flarum/app', 'flarum
             ComposerBody = _flarumComponentsComposerBody['default'];
         }, function (_flarumComponentsComposer) {
             Composer = _flarumComponentsComposer['default'];
-        }, function (_simplemde) {
-            SimpleMDE = _simplemde['default'];
         }],
         execute: function () {
 
